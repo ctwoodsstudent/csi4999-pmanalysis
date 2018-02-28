7eb31db4aafdd10dd773ffb90b51f270067364d2
ctwoodsstudent <ctwoods@oakland.edu>
2018-02-28 15:33:26 -0500

    Valerie, Larisa, and I worked together on very basic search and geo page. Page has no functionality. Page has navbar

--- pmanalysis/pmanalysis/urls.py
+++ pmanalysis/pmanalysis/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^landing/$', views.landing, name='landing'),
+    url(r'^geo/$', views.geo, name='geo'),
+    url(r'^search/$', views.search, name='search'),
     url(r'^signin/$', views.signin, name='signin'),
     url(r'^logged_out/$', views.logout, name='logged_out'),
     url(r'^analysis/$', views.analysis, name='analysis')
