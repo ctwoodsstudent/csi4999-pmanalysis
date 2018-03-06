87fadfdb92da5e2d83aa8c986fc24bdfe588c71f
ctwoodsstudent <ctwoods@oakland.edu>
2018-03-05 22:49:21 -0500

    Test selection popup window. Not connected to anything

--- pmanalysis/static/js/analysisList.js
+++ pmanalysis/static/js/analysisList.js
@@ -18,11 +18,59 @@ function initPage() {
             var selectedRow = e.target.parentElement.parentElement.children[0];
             var fileName = selectedRow.children[0].innerText.split(" ").join("").split("\n").join("");
         }
-        var data = {
+        /*var data = {
             fileName: fileName,
             csrfmiddlewaretoken: csrftoken
-        };
-        $.ajax({
+        };*/
+
+        parent = document.body;
+		var uiContainer = document.createElement("div");
+		uiContainer.className = "uiBody";
+		parent.prepend(uiContainer);
+
+		var headerContainer = document.createElement("div");
+		headerContainer.innerText = "Selected directory: " + fileName;
+		headerContainer.className = "headerClass";
+		uiContainer.append(headerContainer);
+
+		var pValContainer = document.createElement("div");
+		pValContainer.className = "pClass";
+		pValContainer.innerHTML = "Select your P value: " + "<label><input type=\"text\" class=\"myText\" id=\"pVal\" value=\" \"</label>";
+		uiContainer.append(pValContainer);
+
+        var radioControlsContainer = document.createElement("div");
+		radioControlsContainer.className = "radioClass";
+		radioControlsContainer.innerHTML = "<label><input type=\"radio\" name=\"IH-typeSelector\"> Run test 1 (Placeholder name)</label>   <label><input type=\"radio\" name=\"IH-typeSelector\">Run test 2 (Placeholder name)</label>";
+		uiContainer.append(radioControlsContainer);
+
+		//Controls
+		var controlContainer = document.createElement("div");
+		controlContainer.className = "bothButtons";
+
+		var acceptControl = document.createElement("input");
+		acceptControl.type = "button";
+		acceptControl.value = "Start Test";
+		acceptControl.id = "IH-accept";
+		acceptControl.className = "runClass";
+		controlContainer.append(acceptControl);
+
+		var cancelControl = document.createElement("input");
+		cancelControl.type = "button";
+		cancelControl.value = "Cancel";
+		cancelControl.id = "IH-cancel";
+		cancelControl.className = "cancelClass";
+		controlContainer.append(cancelControl);
+
+		uiContainer.append(controlContainer);
+
+		cancelControl.addEventListener("click", function(e){
+		    uiContainer.remove();
+        })
+
+
+
+
+        /*$.ajax({
 			url: window.location.href + "selectItem/",
 			type: "POST",
 			contentType: 'application/json',
@@ -31,14 +79,15 @@ function initPage() {
             headers: {
 			    "X-CSRFToken" : csrftoken
             }
-		}).done(function(response){
-			if (response.success) {
-                alert("selection successful");
-            }
-            else {
-			    alert("selection didn't work");
-            }
-		});
 
+            }).done(function(response){
+                if (response.success) {
+                    alert("selection successful");
+                }
+                else {
+                    alert("selection didn't work");
+                }
+		});
+        */
     })
 }
