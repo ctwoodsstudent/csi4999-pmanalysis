5569b2d1422b8bf01df1d3a90555dc3d2f48f0e7
quentin maltman <quentin.maltman@gmail.com>
2018-03-07 22:08:42 -0500

    py dictionary parser

--- pmanalysis/pmanalysis/parser.py
+++ pmanalysis/pmanalysis/parser.py
@@ -1,28 +1,38 @@
 import csv
 
 def parser(filename, separator):
-    for line in csv.reader(open(Pfile), delimiter=separator, skipinitialspace=True):
+    for line in csv.reader(open(filename), delimiter=separator, skipinitialspace=True):
         if line:
             yield line
 
-
-
-def listerTab(filename):
-    sample = []
-    with open(filename) as f:
-	for line in f:
-		try:
-			probeid, intensity = line.split("\t")
-			sample.append( (str(probeid), float(intensity.strip('\n'))) )
-		except ValueError: pass
-    return sample
+def listerTab(flst):
+    samples = {}   
+    for filename in flst:
+        with open(filename) as f:
+            for line in f:
+                    try:
+                    	probeid, intensity = line.split("\t")
+                    	probeid, intensity = str(probeid), float(intensity.strip('\n'))
+                    	if probeid in samples.keys():
+                            samples[probeid] += [intensity,]
+                        else:
+                            samples[probeid] = [intensity,]
+                    except ValueError: pass
+        f.close()
+    return samples
 
 def listerComma(filename):
-    sample = []
-    with open(filename) as f:
-	for line in f:
-		try:
-			probeid, intensity = line.strip.split(",")
-			sample.append( (str(probeid), float(intensity.strip('\n'))) )
-		except ValueError: pass
-    return sample
+    samples = {}   
+    for filename in flst:
+        with open(filename) as f:
+            for line in f:
+                    try:
+                    	probeid, intensity = line.strip.split(",")
+                    	probeid, intensity = str(probeid), float(intensity.strip('\n'))
+                    	if probeid in samples.keys():
+                            samples[probeid] += [intensity,]
+                        else:
+                            samples[probeid] = [intensity,]
+                    except ValueError: pass
+        f.close()
+    return samples
