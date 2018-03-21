1098fc9e869eb948ff58f2a8354048de29caeab5
kendoncowart <kicowart@oakland.edu>
2018-03-21 14:30:49 -0400

    Change admin site

--- pmanalysis/signup/admin.py
+++ pmanalysis/signup/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserFiles, GEOStudy, Report, UserGEO
 # Register your models here.
+
+admin.site.register(UserFiles)
+admin.site.register(GEOStudy)
+admin.site.register(Report)
+admin.site.register(UserGEO)
